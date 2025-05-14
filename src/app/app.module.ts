8fb579d485abe6e391188298cdd26a9857787655
Harini-sr <harivarshini.sr@gmail.com>
2025-05-14 10:29:54 +0530

    Updated date-time component and added service/component folders

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+
+import {
+  HttpClient,
+  HttpClientModule,
+  provideHttpClient,
+} from '@angular/common/http';
+import { StateManagementComponent } from './component/state-management/state-management.component';
+
 import { AppComponent } from './app.component';
 import { DateTimeComponent } from './time/date-time/date-time.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    DateTimeComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, DateTimeComponent, StateManagementComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  providers: [provideHttpClient()],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/component/state-management/state-management.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-state-management',
+  standalone: false,
+  templateUrl: './state-management.component.html',
+  styleUrl: './state-management.component.css'
+})
+export class StateManagementComponent {
+
+}
